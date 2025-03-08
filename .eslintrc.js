475b87ad2cc23423a7aaeff4165ca389b852cd76
Krechet885 <root37650@gmail.com>
2025-03-08 20:56:30 +0100

    Добавил eslintrc.js, prettierrc, lint-staged.config.js, husky

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  };
---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    '*.{js,jsx}': [
+      'eslint --fix',
+      'prettier --write'
+    ],
+    '*.{json,css,md}': [
+      'prettier --write'
+    ]
+  };
